a4d5aa5146c21fee4b6c1712b9fab137c5ae8c89
ryanbillard <ryan.billard@shopify.com>
2016-10-27 13:08:46 -0400

    implement packet routing for rtprelay and for localrouter rtprelay now listens for an 'initiate' message from stream consumer and then begins routing rtp packets to the client's ports localrouter connects to the relay and routes all packets received to the specified local port needs to be modified to handle both rtp and rtcp ports

--- rtp/localrouter.go
+++ rtp/localrouter.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-  // ec2-54-166-16-218.compute-1.amazonaws.com:5050
+  // ec2-54-166-16-218.compute-1.amazonaws.com:5000
   var listenAddr string
   flag.StringVar(&listenAddr, "listen", ":5000", "Address of server from which rtp stream is requested")
 
-  // var destAddr string
-  // flag.StringVar(&destAddr, "destination", ":8000", "Address to which rtp stream data will be routed")
+  var destAddr string
+  flag.StringVar(&destAddr, "destination", ":5002", "Address to which rtp stream data will be routed")
 
   flag.Parse()
 
@@ -21,13 +21,44 @@ func main() {
   defer serverConn.Close()
   log.Printf("Connected to server at %v", serverConn.RemoteAddr())
 
+  destUDPAddr, err := net.ResolveUDPAddr("udp", destAddr)
+  handleError(err)
+  destConn, err := net.DialUDP("udp", nil, destUDPAddr)
+  handleError(err)
+  defer destConn.Close()
+  log.Printf("Connected to dest port at %v", destConn.RemoteAddr())
+
   _, err = serverConn.Write([]byte("initiate"))
   handleError(err)
-  log.Printf("Sent initializer packet")
+  log.Printf("Sent initializer packet to server")
 
-  buffer := make([]byte, 150000)
-  numRead, err := serverConn.Read(buffer)
-  log.Printf("Received: %v from %v", buffer[:numRead], serverConn.RemoteAddr())
+  log.Printf("Routing messages received on port %v to local port %v", listenAddr, destAddr)
+  packets := make(chan []byte, 1000)
+  go read(serverConn, packets)
+  write(destConn, destUDPAddr, packets)
+}
+
+func read(conn net.Conn, packets chan []byte) {
+  for {
+    buffer := make([]byte, 150000)
+    numRead, err := conn.Read(buffer)
+    if err != nil {
+      log.Fatal(err)
+    }
+    log.Printf("Read %d bytes", numRead)
+    packets <- buffer[:numRead]
+  }
+}
+
+func write(conn *net.UDPConn, addr *net.UDPAddr, packets chan []byte) {
+  for {
+    packet := <- packets
+    log.Printf("Writing %d bytes", len(packet))
+    _, err := conn.Write(packet)
+    if err != nil {
+      log.Fatal(err)
+    }
+  }
 }
 
 func handleError(err error) {
--- rtp/rtprelay.go
+++ rtp/rtprelay.go
@@ -6,18 +6,50 @@ import (
 )
 
 func main() {
+  rtpAddr, err := net.ResolveUDPAddr("udp", ":8000")
+  handleError(err)
+  rtpConn, err := net.ListenUDP("udp", rtpAddr)
+  handleError(err)
+  defer rtpConn.Close()
+
   listenAddr, err := net.ResolveUDPAddr("udp", ":5000")
+  handleError(err)
   clientConn, err := net.ListenUDP("udp", listenAddr)
   handleError(err)
   defer clientConn.Close()
 
   buffer := make([]byte, 150000)
-  numRead, addr, err := clientConn.ReadFromUDP(buffer)
-  log.Printf("Received: %v from %v", buffer[:numRead], addr)
-
-  _, err = clientConn.WriteToUDP([]byte("initiate"), addr)
+  numRead, clientAddr, err := clientConn.ReadFromUDP(buffer)
   handleError(err)
-  log.Printf("Sent initializer packet to client")
+  if string(buffer[:numRead]) == "initiate" {
+    log.Print("Received initiation message from stream consumer")
+  } else {
+    log.Fatal("Received unexpected packet from strem consumer")
+  }
+
+  log.Printf("Routing rtp messages received on port %v to %v", rtpAddr.String(), clientAddr.String())
+  packets := make(chan []byte, 1000)
+  go write(clientConn, clientAddr, packets)
+  read(rtpConn, packets)
+}
+
+func write(conn *net.UDPConn, addr *net.UDPAddr, packets chan []byte) {
+  for {
+    packet := <- packets
+    log.Printf("Writing %d bytes", len(packet))
+    _, err := conn.WriteToUDP(packet, addr)
+    handleError(err)
+  }
+}
+
+func read(conn net.Conn, packets chan []byte) {
+  for {
+    buffer := make([]byte, 150000)
+    numRead, err := conn.Read(buffer)
+    handleError(err)
+    log.Printf("Read %d bytes", numRead)
+    packets <- buffer[:numRead]
+  }
 }
 
 func handleError(err error) {
