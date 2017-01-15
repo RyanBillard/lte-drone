d04044d69bb762d02f6fcd2036387636652176f1
ryanbillard <ryan.billard@shopify.com>
2017-01-14 20:15:58 -0500

    refactor mavproxyrelay to get destination address from received packet

--- mavproxyrelay.go
+++ mavproxyrelay.go
@@ -5,39 +5,35 @@ import (
   "net"
 )
 
-type clientConn struct {
-  conn *net.UDPConn
+type AddressedPacket struct {
   addr *net.UDPAddr
+  data []byte
 }
 
 func main() {
   //listen for MAVLink
-  mavConn := listenOnPort(":8010")
+  droneConn := listenOnPort(":8010")
 
-  //wait for client to initiate
-  mavChan := make(chan clientConn)
-  go waitForClient(":5010", mavChan)
-  
-  clientMav := <- mavChan
+  groundConn := listenOnPort(":5010")
+  //wait for ground station to initiate
+  waitForClient(groundConn)
 
-  log.Printf("Routing mavlink packets received at %v to %v", mavConn.LocalAddr(), clientMav.addr.String())
-  mavPackets := make(chan []byte, 1000)
-  go write(clientMav.conn, clientMav.addr, mavPackets)
-  read(mavConn, mavPackets)
+  log.Print("Routing mavlink packets between drone and ground station")
+
+  incomingPackets := make(chan AddressedPacket, 1000)
+  go write(groundConn, incomingPackets)
+  read(droneConn, incomingPackets)
 }
 
-func waitForClient(addr string, channel chan clientConn) {
-  conn := listenOnPort(addr)
-
+func waitForClient(conn *net.UDPConn) {
   buffer := make([]byte, 100)
-  numRead, clientAddr, err := conn.ReadFromUDP(buffer)
+  numRead, _, err := conn.ReadFrom(buffer)
   handleError(err)
   if string(buffer[:numRead]) == "initiate" {
     log.Print("Received initiation message from stream consumer")
   } else {
     log.Fatal("Received unexpected packet from stream consumer")
   }
-  channel <- clientConn{conn, clientAddr}
 }
 
 func listenOnPort(addr string) *net.UDPConn {
@@ -48,24 +44,24 @@ func listenOnPort(addr string) *net.UDPConn {
   return conn
 }
 
-func write(conn *net.UDPConn, addr *net.UDPAddr, packets chan []byte) {
+func write(conn *net.UDPConn, packets chan AddressedPacket) {
   defer conn.Close()
   for {
     packet := <- packets
-    log.Printf("Writing %d bytes", len(packet))
-    _, err := conn.WriteToUDP(packet, addr)
+    log.Printf("Writing %d bytes", len(packet.data))
+    _, err := conn.WriteToUDP(packet.data, packet.addr)
     handleError(err)
   }
 }
 
-func read(conn net.Conn, packets chan []byte) {
+func read(conn *net.UDPConn, packets chan AddressedPacket) {
   defer conn.Close()
   for {
     buffer := make([]byte, 150000)
-    numRead, err := conn.Read(buffer)
+    numRead, addr, err := conn.ReadFromUDP(buffer)
     handleError(err)
     log.Printf("Read %d bytes", numRead)
-    packets <- buffer[:numRead]
+    packets <- AddressedPacket{addr, buffer[:numRead]}
   }
 }
 
