743d32f639d0674e748d307134e0d032ed253267
ryanbillard <ryan.billard@shopify.com>
2017-01-19 11:22:04 -0500

    change script to install go packages so that they don't put executable in repository

---
+++ scripts/installAllPackages.sh
@@ -0,0 +1,6 @@
+set -e
+go install github.com/RyanBillard/lte-drone/shared
+go install github.com/RyanBillard/lte-drone/groundmav
+go install github.com/RyanBillard/lte-drone/groundrtp
+go install github.com/RyanBillard/lte-drone/relaymav
+go install github.com/RyanBillard/lte-drone/relayrtp
